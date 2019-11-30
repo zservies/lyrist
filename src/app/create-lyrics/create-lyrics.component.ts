699bffa699a42857cb180ac8efcb948b00ce57d8
zservies <zservies@purdue.edu>
2019-11-30 02:27:15 -0500

    Added app-routing. Created create-lyrics component and added title, author, and lyric boxes.

---
+++ src/app/create-lyrics/create-lyrics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-lyrics',
+  templateUrl: './create-lyrics.component.html',
+  styleUrls: ['./create-lyrics.component.scss']
+})
+export class CreateLyricsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
